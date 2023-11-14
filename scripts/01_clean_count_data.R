9f0202290e6eb6db390e45f82b3cd95b0e8c764b
yasmine hentati <yhentati26@gmail.com>
2023-11-14 10:09:41 -0800

    worked on cleaning and urbanization scripts

--- scripts/01_clean_count_data.R
+++ scripts/01_clean_count_data.R
@@ -32,7 +32,7 @@ se_counts <- left_join(se_caps, se_meta, by = c("station","season"))
 
 
 # read in data
-all_data <- read_csv("data/raw_data_from_uwin/yasmine_west_coast_data.csv") 
+all_data <- read_csv("data/raw_data_from_uwin/yasmine_tawa_19_20.csv") 
 length(unique(all_data$locationAbbr))
 class(all_data)
 
@@ -42,7 +42,7 @@ nrow(all_data)
 unique(all_data$commonName)
 
 # read in detection history data set
-detections <- read_csv("data/raw_data_from_uwin/initial_data_yasmine.csv")
+detections <- read_csv("data/raw_data_from_uwin/yasmine_samps.csv")
 
 # remove all sites with 0 days running from detection data
 detections <- detections[detections$J != 0, ]
@@ -59,55 +59,79 @@ colnames(counts)
 
 # take site info
 all_data <- all_data %>%
-  dplyr::select(locationID, locationAbbr, utmEast, utmNorth, utmZone) %>% distinct()
+  dplyr::select(locationID, locationAbbr, longitude, latitude) %>% distinct()
 head(all_data)
 
 # join counts to site data 
-joined_data <- left_join(counts, all_data, by = "locationID")
-nrow(joined_data)
-length(unique(joined_data$locationAbbr))
-
-joined_data
-# now we have counts for each site/season combo 
-# but we only want tacoma data for this project
-head(joined_data)
-ta_counts <- joined_data %>% filter(city == "TAWA")
-  
-head(ta_counts)
-
-# let's change UTM to lat long
-
-# first make all of our zones just 10
-
-ta_counts$utmZone[ta_counts$utmZone == "10N"] <- 10
-
-unique(ta_counts$utmZone)
-
-# get lat long for all points in zone 10
-utm1 <- data.frame(x=ta_counts$utmEast, y=ta_counts$utmNorth) 
-coordinates(utm1) <- ~x+y 
-class(utm1)
-proj4string(utm1) <- CRS("+proj=utm +zone=10 +datum=WGS84 +units=m +ellps=WGS84") 
-utm2 <- spTransform(utm1,CRS("+proj=longlat +datum=WGS84"))
-utm2
-
-ta_counts$long <- utm2$x
-ta_counts$lat <- utm2$y
-
+ta_counts <- left_join(counts, all_data, by = "locationID")
 nrow(ta_counts)
+length(unique(ta_counts$locationAbbr))
+# now we have counts for each site/season combo 
+
 
 
 # change all city into lowercase
 ta_counts$city <- tolower(ta_counts$city)
 
 # change colnames
-colnames(ta_counts) <- c("Species", "locationID", "city", "season",
-                       "count", "Site", "utmEast", "utmNorth",
-                       "utmZone", "long", "lat")
+colnames(ta_counts) <- c("species", "locationID", "city", "season",
+                       "count", "Site", "long", "lat")
 
 head(ta_counts)
 ################################################################################
 ## removing/merging sites 
+
+
+
+
+
+# use mason's functions to collapse sites by location 
+source("scripts/mason_site_collapse_code/qaqc_sites.R")
+source("scripts/mason_site_collapse_code/long_to_zone.R")
+source("scripts/mason_site_collapse_code/fix_site_names.R")
+
+new_dat_1 <- qaqc_sites(x = ta_counts, cities="city", sites = "Site",
+                        my_coords=c("long","lat"), my_crs=4326)
+
+# this function will tell us what sites need to be merged or removed based
+# on proximity to the next site(s)
+# the "remove" process may not be totally accurate for this data set because it is 
+# made to keep the site with more data in an occupancy data set, but 
+# we have counts. for now we will keep these separate 
+
+fix_site_names(new_dat_1)
+
+# manually decide which sites to remove from count data after comparing 
+
+# merge  
+# P-TE2 and P-TE1 
+# P-GW1, P-GW2, P-GW3
+# P-MKP2, P-MKP3
+# P-OV1, P-OV2
+# G-CBP1, G-CBP2
+# G-KP1, G-KP2
+# G-WLP1, G-WLP2
+# G-FN1, G-FN2
+# G-TNC1, G-TNC2
+
+# this is based on the fix_site_names function 
+
+ta_counts$Site[ta_counts$Site == "P-TE2"] <- "P-TE1 " # keep
+ta_counts$Site[ta_counts$Site == "P-GW2"] <- "P-GW1" # keep
+ta_counts$Site[ta_counts$Site == "P-GW3"] <- "P-GW1" # keep
+ta_counts$Site[ta_counts$Site == "P-MKP3"] <- "P-MKP2" # keep
+ta_counts$Site[ta_counts$Site == "P-OV2"] <- "P-OV1" # keep
+ta_counts$Site[ta_counts$Site == "G-CBP2"] <- "G-CBP1" # keep
+ta_counts$Site[ta_counts$Site == "G-KP2"] <- "G-KP1" # keep
+ta_counts$Site[ta_counts$Site == "G-WLP2"] <- "G-WLP1" # keep
+ta_counts$Site[ta_counts$Site == "G-FN2"] <- "G-FN1" # keep
+ta_counts$Site[ta_counts$Site == "G-TNC2"] <- "G-TNC1" # keep
+ta_counts$Site[ta_counts$Site == "P-TE2"] <- "P-TE1"
+
+
+
+
+
 
 
 # we will remove sites that don't have enough data in detection data
@@ -119,10 +143,14 @@ sites_det_only <- setdiff(unique(detections$Site),unique(ta_counts$Site))
 # now see which sites are not present in detection data 
 sites_count_only <- setdiff(unique(ta_counts$Site),unique(detections$Site))
 
+sites_count_only
 
+### G-AGC, GKP1, G-FN1, G-TCC1 not in detection data 
+
+View(detections)
 # remove all sites that don't exist in detection dataset 
 
-new_dat_cut <- subset(ta_counts, Site != "P-MCP")
+new_dat_cut <- subset(ta_counts)
 new_dat_cut
 
 # now remove all seasons of a site that don't exist in detection data set 
@@ -133,13 +161,13 @@ uniques <- arrange(uniquesiteseason, Site, Season)
 table(uniquesiteseason$Site)
 
 # same with counts 
-uniquesiteseason2 <- unique(new_dat_cut[c("Site", "season")])
+uniquesiteseason2 <- unique(ta_counts[c("Site", "season")])
 table(uniquesiteseason2$Site)
 
 # let's remove oct 21 since it doesn't exist in detections data and is probably
 # incomplete 
 
-ta_counts <- subset(new_dat_cut, season != "OC21")
+ta_counts <- subset(ta_counts, season != "OC21")
 
 ################################################################################
 
@@ -235,7 +263,7 @@ se_counts$Species[se_counts$Species == "Ursus americanus"] <- "Black bear"
 
 wa_counts <- rbind(ta_counts, se_counts)
 
-
 nrow(ta_counts)
-View(se_counts)
-nrow(se_counts)
+
+View(wa_counts)
+
--- scripts/02_urb_metrics.R
+++ scripts/02_urb_metrics.R
@@ -1,6 +1,213 @@
 ################################################################################
 ################# Washington Env Health GLMM Analysis  #########################
-################# Step 2A: Calculate Species Richness, #########################
-#################     Diversity, & Abundance           #########################
+################# Step 2: urbanization metrics         #########################
 #################   Yasmine Hentati yhentati@uw.edu    #########################
 ################################################################################
+
+library(here)
+library(readr)
+library(dplyr)
+library(sf)
+library(mapview)
+## packages 
+library(raster)
+library(sf)
+library(dplyr)
+library(mapview) # note that mapview is somewhat demanding, consider skipping
+# mapview functions if your computer is slow  - these are just used to check 
+# that shapefiles look correct 
+library(rgdal)
+library(raster)
+library(here)
+library(remotes)
+library(tidycensus)
+# remotes::install_github("walkerke/crsuggest") 
+library(crsuggest)
+library(tidyr)
+library(terra)
+install.packages("spatialEco")
+library(spatialEco)
+library(readr)
+library(ggfortify)
+library(rgeos)
+install.packages("exactextractr")
+library(exactextractr)
+
+##### get data from lat/long to utms 
+
+
+# load all sites
+all_sites <- read.csv(here("data", "wa_counts.csv"), stringsAsFactors = FALSE)
+
+# order data by Location Name 
+all_sites <- all_sites[order(all_sites$site),]
+nrow(all_sites)
+head(all_sites)
+
+# create new column for utm zone 
+
+# transform into UTM - WA
+# all_sites <- data.frame(x=all_sites$long, y=all_sites$lat) 
+all_sites <- st_as_sf(all_sites, coords=c("long", "lat"), crs="EPSG:4326")
+all_sites <- st_transform(all_sites, crs = "EPSG:32610")
+
+head(all_sites)
+
+all_sites <- all_sites %>%
+  dplyr::mutate(utm_east = sf::st_coordinates(.)[,1],
+                utm_north = sf::st_coordinates(.)[,2])
+head(all_sites)
+
+class(all_sites)
+
+# only keep 1 row for each site 
+points_wa <- all_sites %>% distinct(site, .keep_all = TRUE) 
+
+head(points_wa)
+
+# check that it looks ok
+mapview(points_wa)
+
+
+
+
+################################################################################
+## INCOME DATA
+## median household income data from American Community Survey
+
+Sys.getenv("CENSUS_API_KEY")
+# load census API key
+# census_api_key("e649b78a1d98fe7e1c9ff7039840781976777eb6",
+ #    install = TRUE)
+# readRenviron("~/.Renviron")
+
+# load in ACS variables  from 2019 
+v17 <- load_variables(2019, "acs5", cache = TRUE)
+
+
+# load in shapefile of subdivisions  
+# with median household income as variable of interest
+
+# for washington 
+tractincomeWA <- get_acs(state = "WA", 
+                         geography = "tract", 
+                         variables = c(medincome="B19013_001"), geometry = TRUE)
+
+# cut out everything except king and pierce county from our WA shp 
+# specify dplyr because rgdal also has filter fxn 
+# do this using the GEOID codes for each county 
+
+tractsKP <- tractincomeWA %>% dplyr::filter(substr(GEOID, 1, 5) 
+                                            %in% c("53033", "53053"))
+
+# mapview(tractsKP) # looks good
+
+
+# crop to actual study area 
+tractsKP <- st_transform(tractsKP, crs=4326)
+tractsKP_crop <- st_crop(tractsKP, c(xmin= -121.7, ymin = 46.7, xmax = -122.8, ymax = 47.8))
+mapview(tractsKP_crop)
+
+
+# write shapefiles for cropped area 
+st_write(tractsKP_crop, here("data", "income_maps", "seatac_urban_med_income.shp"),
+     append = FALSE)
+
+
+
+################################################################################
+## HOUSING DENSITY DATA 
+## read in housing density  data - starting with WA 
+# data from: ???
+
+wa_housing <- st_read(here("data", "housing_maps", 
+                           "wa_blk10_Census_change_1990_2010_PLA2.shp"))
+
+# filter to only king and pierce county
+wa_housing <- wa_housing %>% dplyr::filter(substr(BLK10, 1, 5) 
+                                           %in% c("53033", "53053"))
+
+# we only need 2010 housing data - select relevant info
+
+colnames(wa_housing)
+wa_housing <- wa_housing %>% dplyr::select(BLK10, WATER10, POP10, 
+                                           HU10, HUDEN10, HHUDEN10,
+                                           PUBFLAG:geometry)
+colnames(wa_housing)
+
+# crop to actual study area 
+sf_use_s2(FALSE)
+wa_housing <- st_transform(wa_housing, crs=4326)
+wa_housing <- st_crop(wa_housing, c(xmin= -121.7, ymin = 46.7, xmax = -122.8, ymax = 47.8))
+
+# mapview(wa_housing)
+
+st_write(wa_housing, here("data", "housing_maps", "wa_urban_huden_2010.shp"),
+    append = FALSE)
+
+
+
+
+
+################################################################################
+## VEGETATION DATA 
+
+
+################################################################################
+## ENVIRONMENTAL HEALTH DATA 
+# obtained from washington environmental health disparities map
+
+
+waenv1 <- read.csv(here("data", "WA Environmental Effects.csv"))
+waenv2 <- read.csv(here("data", "WA Environmental Exposures.csv"))
+
+waenv3 <- merge(waenv1, waenv2, by = "State.FIPS.Code")
+head(waenv3)
+
+# average effects and exposures
+waenv3$Rank <- rowMeans(waenv3[,c('Rank.x', 'Rank.y')])
+waenv <- waenv3 %>% dplyr::select(-c("Rank.x","Rank.y"))
+head(waenv)
+
+#get only our counties of interest 
+waenv <- waenv %>% dplyr::filter(substr(State.FIPS.Code, 1, 5) 
+                                 %in% c("53033", "53053", # king pierce 
+                                        "53061", "53035", # snoho kitsap
+                                        "53029", "53057")) # island skagit
+
+# rename col names to match
+colnames(waenv) <- c("GEOID", "Rank")
+colnames(calenv) <- c("GEOID", "Rank")
+
+write_csv(waenv, here("data", "env_health_ranks_wa2.csv"))
+
+################################################################################
+## impervious surface calculation 
+library(here)
+
+# load impervious cover raster map
+imp_map <- rast(here("data", "NLCD_imp", 
+                       "nlcd_2019_impervious_descriptor_l48_20210604.img"))
+
+# again reproject  points to match raster
+# these are just the points not the buffers
+
+points_wa <- points_wa[!st_is_empty(points_wa),]
+points_wa <- as(points_wa, "Spatial")
+
+
+#create a buffer around the points
+sp_buffer <-st_buffer(st_as_sf(points_wa),500) 
+
+head(points_wa)
+
+
+# reproject impervious surface map
+imp_map <- terra::project(imp_map,crs(points_wa))
+
+# extract the mean impervious cover around each point using 500 m radius buffer
+imp <- terra::extract(imp_map, points_wa, buffer = 500, fun=mean, df=TRUE)
+
+imp
+?terra::extract
+points_WA$imp_surf <- imp$nlcd_2019_impervious_descriptor_l48_20210604
